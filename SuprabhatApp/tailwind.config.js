e54f70dfe1cd4fcb0c0b9f1f9bbf36f45858bf91
w3-80717 <chavan1609@gmail.com>
2025-11-07 22:54:17 +0530

    solved some of the tailwind problems in react native

--- SuprabhatApp/tailwind.config.js
+++ SuprabhatApp/tailwind.config.js
@@ -1,12 +1,14 @@
 
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  // NOTE: Update this to include the paths to all of your component files.
-  content: ["./App.{js,jsx,ts,tsx}", "./screens/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    /** @type {import('tailwindcss').Config} */
+    module.exports = {
+      content: [
+        "./src/**/*.{js,jsx,ts,tsx}", // Example: include your components folder
+      ],
+      presets: [require("nativewind/preset")],
+      theme: {
+        extend: {},
+      },
+      plugins: [],
+    };
