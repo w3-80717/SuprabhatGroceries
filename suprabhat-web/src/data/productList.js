d35198ba6e1c5673f6ff0798c0c50a9b5691a246
w3-80717 <chavan1609@gmail.com>
2025-11-07 15:24:12 +0530

    coriander image add

--- suprabhat-web/src/data/productList.js
+++ suprabhat-web/src/data/productList.js
@@ -1,7 +1,7 @@
 const products = [
   { id: 1, name: 'Tomatoes', price: 60, image: 'public/images/tomato.jpeg', unit: 'kg', stock: 20 },
   { id: 2, name: 'Potatoes', price: 30, image: '/images/potato.jpeg', unit: 'kg', stock: 30 },
-  { id: 3, name: 'Wheat Flour', price: 45, image: 'https://via.placeholder.com/300?text=Flour', unit: 'kg', stock: 10 },
+  { id: 3, name: 'Coriander', price: 45, image: 'public/images/coriander.jpeg', unit: 'kg', stock: 10 },
   { id: 4, name: 'Sugar', price: 40, image: 'https://via.placeholder.com/300?text=Sugar', unit: 'kg', stock: 15 },
   { id: 5, name: 'Tea Powder', price: 90, image: 'https://via.placeholder.com/300?text=Tea', unit: '500g', stock: 8 },
 ];
