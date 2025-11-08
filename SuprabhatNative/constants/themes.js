a61964f00fb911e23d911d57a6f3a64249058679
w3-80717 <chavan1609@gmail.com>
2025-11-08 14:44:23 +0530

    theme added

---
+++ SuprabhatNative/constants/themes.js
@@ -0,0 +1,20 @@
+export const COLORS = {
+    brandGreen: '#2a623d',
+    brandGreenLight: '#3a8654',
+    brandBeige: '#f5f5dc',
+    brandText: '#3a3a3a',
+    brandAccent: '#ff8c42',
+    white: '#FFFFFF',
+    black: '#000000',
+    red: '#FF0000',
+    gray: '#808080',
+};
+export const SIZES = {
+    base: 8,
+    small: 12,
+    font: 14,
+    medium: 16,
+    large: 18,
+    xl: 24,
+    xxl: 30,
+};
